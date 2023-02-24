7ed28cb650ba909322fde8156151f02bd14464fa
erlenkb <erlenkb@stud.ntnu.no>
2023-02-24 14:46:44 +0100

    Added Propagation function for P and S

--- .venv/TLM matrix.jl
+++ .venv/TLM matrix.jl
@@ -120,6 +120,23 @@ function create_shoebox(Δd, height, width, length)
 end
 
 
+function case(n::Int)
+    Diffusor = false
+    if n < 0
+        Diffusor = true
+        n = abs(n)
+    end
+    num_str = string(num)
+    used_nums = [parse(Int, digit) for digit in num_str]
+    all_nums = set(1:6)
+    unused_nums = setdiff(collect(all_nums), used_nums)
+
+    return used_nums, unused_nums, Diffusor
+end
+
+
+"""
+Old function to handle case parsing. --> Did only consider used labels, changed with function above that consider both used and unused
 
 function case(n::Integer)
     Diffusor = false
@@ -131,13 +148,13 @@ function case(n::Integer)
     while n > 0
         digit = n % 10
         push!(digits_set, digit)
-        n = div(n, 10)
+        n = div(n, 10)    
     end
 
 
     return digits_set, Diffusor
 end
-
+"""
 
 function mark_box_walls(arr::Array{Float64,3}, d, corners)
     for i in 1:size(arr, 1)
@@ -262,6 +279,70 @@ function place_wall(arr::Array{Float64, 3}, x_pos::Array{Float64, 1}, y_pos::Arr
     return arr
 end
 
+function calculate_pressure_matrix(Labeled_tlm::Array{Float64,3}, SN::Array{Float64,3}, SE::Array{Float64,3}, SS::Array{Float64,3}, SW::Array{Float64,3}, SU::Array{Float64,3}, SD::Array{Float64,3},PN::Array{Float64,3}, PE::Array{Float64,3}, PS::Array{Float64,3}, PW::Array{Float64,3}, PU::Array{Float64,3}, PD::Array{Float64,3})
+    for i in 1:size(Labeled_tlm,1)
+        for j in 1:size(Labeled_tlm,2)
+            for k in 1:size(Labeled_tlm,3)
+                if Labeled_tlm[i,j,k] == 0
+                    PN[i,j,k] = SS[i - 1, j, k]
+                    PE[i,j,k] = SW[i, j + 1, k]
+                    PS[i,j,k] = SN[i + 1, j, k]
+                    PW[i,k,k] = SE[i, j - 1, k]
+                    PU[i,j,k] = SD[i, j, k + 1]
+                    PD[i,j,k] = SU[i, j, k - 1]
+                    continue
+                else
+                    case_used, case_unused, diffusor = case(Labeled_tlm[i,j,k])
+                    
+                    for n in case_used
+                        Refl = diffusor ? R[6] : R[n]
+                        if n == 1
+                            PN[i,j,k] = Refl * SN[i,j,k]                        
+                        elseif n == 2
+                            PS[i,j,k] = Refl * SS[i,j,k]
+                        elseif n == 3
+                            PW[i,j,k] = Refl * SW[i,j,k]
+                        elseif n == 4
+                            PE[i,j,k] = Refl * SE[i,j,k]
+                        elseif n == 5
+                            PD[i,j,k] = Refl * SD[i,j,k]
+                        elseif n == 6
+                            PU[i,j,k] = Refl * SU[i,j,k]
+                        end
+                    end
+                    for n in case_unused
+                        if n == 1
+                            PN[i,j,k] = SS[i - 1, j, k]
+                        elseif n == 2
+                            PS[i,j,k] = SN[i + 1, j, k]
+                        elseif n == 3
+                            PW[i,k,k] = SE[i, j - 1, k]
+                        elseif n == 4
+                            PE[i,j,k] = SW[i, j + 1, k]
+                        elseif n == 5
+                            PD[i,j,k] = SU[i, j, k - 1]
+                        elseif n == 6
+                            PU[i,j,k] = SD[i, j, k + 1]
+                        end
+                    end
+                end
+            end
+        end
+    end
+    return SN, SE, SS, SW, SU, SD, PN, PE, PS, PW, PU, PD
+end
+
+
+function calculate_scattering_matrix(SN::Array{Float64,3}, SE::Array{Float64,3}, SS::Array{Float64,3}, SW::Array{Float64,3}, SU::Array{Float64,3}, SD::Array{Float64,3},PN::Array{Float64,3}, PE::Array{Float64,3}, PS::Array{Float64,3}, PW::Array{Float64,3}, PU::Array{Float64,3}, PD::Array{Float64,3})
+    SW = (1/3) * (-2*PW .+ PN .+ PE .+ PS .+ PU .+ PD)
+    SN = (1/3) * (PW .- (2*PN) .+ PE .+ PS .+ PU .+ PD)
+    SE = (1/3) * (PW .+ PN .- (2*PE) .+ PS .+ PU .+ PD)
+    SS = (1/3) * (PW .+ PN .+ PE .- (2*PS) .+ PU .+ PD)
+    SD = (1/3) * (PW .+ PN .+ PE .+ PS .- (2*PU) .+ PD)
+    SU = (1/3) * (PW .+ PN .+ PE .+ PS .+ PU .- (2*PD))
+end
+
+
 """
 # First try at replacing surfaces with the value 1 ---- trashed due to unecessary complexity
 
@@ -277,6 +358,7 @@ function replace_edges_with_one(arr::AbstractArray{T,N}) where {T,N}
 end
 
 """
+
 
 
 
@@ -296,8 +378,8 @@ z2 = [0.19,0.]
 
 
 IWE = create_shoebox(Δd, 12, 12, 12)
-IWE2 = place_wall(IWE, x, y, z, Δd)
-IWE3 = place_wall(IWE2, x2, y2, z2, Δd)
+IWE2 = place_wall(IWE, x2, y2, z2, Δd)
+#IWE3 = place_wall(IWE2, x2, y2, z2, Δd)
 
 
 
